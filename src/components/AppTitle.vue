f803c3e7ddf4a461d34b4c809f38122135b34706
EnricoDErasmo <enrico.derasmo@gmail.com>
2023-03-23 16:05:47 +0100

    Excercise completed

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+
+</script>
+
+<template>
+    <h1 class="title">
+        La mia prima app con Vite!
+    </h1>
+</template>
+
+<style>
+.title {
+    font-size: 3em;
+    text-align: center;
+}
+</style>
